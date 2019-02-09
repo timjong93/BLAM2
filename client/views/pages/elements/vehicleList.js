45d00dbcae48942bce5bfd618e1b93a806e22e8d
danny.verpoort <danny.verpoort@takeaway.com>
2019-02-09 20:29:28 +0100

    Verhicle sidebar

--- client/views/pages/elements/vehicleList.js
+++ client/views/pages/elements/vehicleList.js
@@ -1,5 +1,11 @@
 Template.vehicleList.open = false
 
+Template.vehicleList.rendered = function(){
+  $('.sidebar-right .vehicleList').slimScroll({
+    height: '40vh'
+  })
+}
+
 Template.vehicleList.events({
   'click .sidebar-right-trigger' (event) {
     if(Template.vehicleList.open){
