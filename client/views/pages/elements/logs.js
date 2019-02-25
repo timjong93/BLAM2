f2bff3565197a3da48b8b5877e930b747ae937e8
tim jongsma <mail@tjongsma.nl>
2019-02-25 18:58:19 +0100

    add labels to users in log list

--- client/views/pages/elements/logs.js
+++ client/views/pages/elements/logs.js
@@ -138,8 +138,12 @@ Template.logs.helpers({
 		}
 		if (logs) {
 			logs.map(function(log){
+				let users = Meteor.users.find().fetch().map((u)=>{return u.username});
+				let userRegex = new RegExp('('+users.join('|')+')','ig');
+				
 				log.message = log.message.replace(/(BATA-[0-9]*)/ig, '<span class="label label-primary">$1</span>');
-	        	log.ticket = Tickets.findOne({logs:log._id}, {fields: {title:1}});
+				log.message = log.message.replace(userRegex, '<span class="label label-warning">$1</span>');
+	      log.ticket = Tickets.findOne({logs:log._id}, {fields: {title:1}});
 				return log;
 			});
 			return logs;
